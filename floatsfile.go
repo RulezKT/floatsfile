8ec8dc4159555a4ec9c18246704640873cacda42
RR <9381804@gmail.com>
2025-02-02 20:09:01 +0100

    first

---
+++ floatsfile.go
@@ -0,0 +1,28 @@
+package floatsfile
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+)
+
+func LoadBinary(path string, floats64Num int) []float64 {
+
+	f, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	r := bytes.NewReader(f)
+	floatArr := make([]float64, floats64Num)
+
+	err = binary.Read(r, binary.LittleEndian, &floatArr)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+
+	return floatArr
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module floatsfile
+
+go 1.22.2
