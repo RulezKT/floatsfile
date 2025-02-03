dfab1a57117054a11717959d35f1abd1ff839e55
RR <9381804@gmail.com>
2025-02-03 01:39:50 +0100

    added int64 load

--- floatsfile.go
+++ floatsfile.go
@@ -26,3 +26,22 @@ func LoadBinary(path string, floats64Num int) []float64 {
 
 	return floatArr
 }
+
+func LoadIntBinary(path string, floats64Num int) []int64 {
+
+	f, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	r := bytes.NewReader(f)
+	intArr := make([]int64, floats64Num)
+
+	err = binary.Read(r, binary.LittleEndian, &intArr)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+
+	return intArr
+}
